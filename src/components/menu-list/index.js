68328ae46797f764cdb297d4b3edefc7acc5a698
shuang.huang <shuang.huang@envision-digital.com>
2022-01-07 14:07:20 +0800

    菜单高度占满

--- src/components/menu-list/index.js
+++ src/components/menu-list/index.js
@@ -3,7 +3,7 @@ import SubMenu from "antd/lib/menu/SubMenu";
 import React from "react";
 export default function MenuList() {
     return (
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['mobx']}>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['mobx']} style={{height: 'calc(100vh - 64px)'}}>
             <SubMenu title="学习">
                 <Menu.Item key='mobx'>{'Mobx'}</Menu.Item>
                 <Menu.Item key='other'>{'other'}</Menu.Item>
