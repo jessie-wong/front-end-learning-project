ba196c9ee8d0712f8dce5137f26cc6c59de1cc10
jessie-wong <766131907@qq.com>
2022-01-27 00:05:00 +0800

    简陋的表格轮播图

--- src/pages/css-layout/grid-layout.js
+++ src/pages/css-layout/grid-layout.js
@@ -1,7 +1,5 @@
 // 导入包
 import React, { useEffect, useState } from 'react';
-import GridLayout from './grid-layout';
-import ScrollTable from './learn-css/scroll-table';
 import './style.less';
 
 // HTML是最优秀的标记语言；
@@ -10,12 +8,23 @@ import './style.less';
 // 这种在JS中混合写入类似于HTML的语法叫做JSX语法，符合XML规范的JS
 // JSX语法的本质还是在运行的时候，被babel转换成React.createElement形式来执行的
 
-export default function CSSLayout() {
+export default function GridLayout() {
 
   return (
     <div>
-      {/* <GridLayout /> */}
-      <ScrollTable />
+      <div className='container'>
+        <div className='header1'></div>
+        <div className='header2'></div>
+        <div className='header3'></div>
+        <div className='header4'></div>
+        <div className='sider1'></div>
+        <div className='sider2'></div>
+        <div className='map'></div>
+        <div className='sider3'></div>
+        <div className='sider4'></div>
+        <div className='botton1'></div>
+        <div className='botton2'></div>
+      </div>
     </div>
   );
 }
